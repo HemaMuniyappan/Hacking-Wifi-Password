5b7784f501271efaa91a2ecb338b4ec4326900f8
HemaMuniyappan <117437208+HemaMuniyappan@users.noreply.github.com>
2024-01-29 21:59:57 +0530

    Add files via upload

---
+++ wifi password.py
@@ -0,0 +1,48 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+Traceback (most recent call last):
+  File "C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py", line 1, in <module>
+    data = (subprocess.check_output(["netsh","wlan"])
+NameError: name 'subprocess' is not defined
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+Traceback (most recent call last):
+  File "C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py", line 2, in <module>
+    subprocess.check_output(["netsh","wlan"])
+NameError: name 'subprocess' is not defined
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+Traceback (most recent call last):
+  File "C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py", line 2, in <module>
+    subprocess.check_output(["netesh","wlan"])
+NameError: name 'subprocess' is not defined
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+Traceback (most recent call last):
+  File "C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py", line 2, in <module>
+    subprocess.check_output(["netsh","wlan"])
+NameError: name 'subprocess' is not defined
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+Traceback (most recent call last):
+  File "C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py", line 2, in <module>
+    subprocess.check_output(["netsh", "wlan", "show", "profiles"])
+NameError: name 'subprocess' is not defined
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+CIT-Campus                    | citchennai
+Hema_5G                       | 210604karthika
+Oppo                          | 12345678
+oppo                          | 12345678
+airtel200mbs                  | asus@54321
+Mogs                          | 9941379919
+>>> 
+==================================================== RESTART: C:/Users/Asus/AppData/Local/Programs/Python/Python311/wifi password.py ===================================================
+CIT-Campus                    | citchennai
+Hema_5G                       | 210604karthika
+Oppo                          | 12345678
+oppo                          | 12345678
+airtel200mbs                  | asus@54321
+Mogs                          | 9941379919
